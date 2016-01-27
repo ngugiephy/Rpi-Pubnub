9a2dd757ab68cc1542b0b6ef11f8a706495d1358
ngugiephy <ngugiephy@gmail.com>
2016-01-27 23:49:29 +0300

    Update led.py

--- led.py
+++ led.py
@@ -1,2 +1,21 @@
+##    ngugiephy
+##
+##  simple led blink for the raspberry pi
+
 import RPi.GPIO as GPIO
-import pubnub
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+led = 4
+GPIO.setup(led, GPIO.OUT, initial=0)
+
+try:
+  while True:
+    GPIO.output(led, 1)
+    time.sleep(0.1)
+    GPIO.output(led, 0)
+    time.sleep(0.1)
+    
+finally:
+  GPIO.cleanup()
