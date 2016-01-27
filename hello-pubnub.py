0d3ebada5b74ac93adad3aaf50ebf51dff0b9dc6
ngugiephy <ngugiephy@gmail.com>
2016-01-27 23:07:09 +0300

    Update hello-pubnub.py

--- hello-pubnub.py
+++ hello-pubnub.py
@@ -1,2 +1,16 @@
 import sys
 from pubnub import Pubnub
+
+pubnub = Pubnub(publish_key='', subscribe_key='')
+
+channel = "hello-pi"
+
+data = {
+  'username': 'ngugiephy',
+  'message' : "hey there, it's ngugephy"
+  }
+def callback(m):
+  print(m)
+  
+pubnub.publish(channel, data, callback=callback, error=callback)
+  
